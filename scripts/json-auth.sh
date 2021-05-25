dfe2535a06f2cafbadf2eaf1088dca502abdea03
Zachary Conger <zachary@econger.com>
2021-05-25 00:15:48 -0600

    move things around and simplify

--- scripts/json-auth.sh
+++ scripts/json-auth.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
 # Fetch a JWT token from JavaSpringVulny using the JSON signin endpoint
-RESPONSE=$(curl --location --request POST 'https://localhost:9000/api/jwt/auth/signin' \
+RESPONSE=$( curl --request POST 'https://localhost:9000/api/jwt/auth/signin' \
     --header 'Content-Type: application/json' \
-    --silent \
-    --insecure \
-    --data-raw '{
-        "password":	"password",
-        "username":	"user"
-    }')
+    --data-raw '{"password": "password", "username": "user"}' \
+    --silent --insecure )
 
 # Extract the JWT token from the JSON response
 echo $RESPONSE | jq '.token' -r
