787dfd8c7608d0c033682cca2fbf92aca4f20a16
Scott Gerlach <scott.gerlach@stackhawk.com>
2021-10-28 12:20:44 -0600

    Adding postgres and docker configs and enabling detailed errors

--- src/main/java/hawk/service/SearchService.java
+++ src/main/java/hawk/service/SearchService.java
@@ -13,24 +13,33 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class SearchService {
-
+    private static final Logger LOGGER = Logger.getLogger( SearchService.class.getName() );
     @Autowired
     EntityManager entityManager;
 
     public List<Item> search(Search search) {
+        LOGGER.setLevel(Level.FINE);
+
         final Session session = (Session) entityManager.unwrap(Session.class);
         List items = session.doReturningWork(new ReturningWork<List<Item>>() {
             @Override
             public List<Item> execute(Connection connection) throws SQLException {
                 List<Item> items = new ArrayList<>();
+                String query = "select id, name, description from ITEM where description like '%" +
+                        search.getSearchText() + "%'";
+
+                LOGGER.log(Level.INFO, "SQL Query " + query);
                 ResultSet rs = connection
                         .createStatement()
-                        .executeQuery(
-                                "select * from ITEM where name like '%" + search.getSearchText() + "%'"
-                        );
-                //or description like '%" + search.getSearchText() + "%'
+                        .executeQuery(query);
+
                 while (rs.next()) {
                     items.add(new Item(rs.getLong("id"), rs.getString("name"), rs.getString("description")));
                 }
