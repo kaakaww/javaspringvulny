bfdf986be59d5de42a9cf05fb929d912d4f06654
Scott Gerlach <scott.gerlach@stackhawk.com>
2021-11-02 09:22:25 -0600

    Fixing security problems, XSS, SQLi

--- src/main/java/hawk/service/SearchService.java
+++ src/main/java/hawk/service/SearchService.java
@@ -5,18 +5,16 @@ import hawk.form.Search;
 import org.hibernate.Session;
 import org.hibernate.jdbc.ReturningWork;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import javax.persistence.EntityManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.*;
+
 
 public class SearchService {
     private static final Logger LOGGER = Logger.getLogger( SearchService.class.getName() );
@@ -31,14 +29,12 @@ public class SearchService {
             @Override
             public List<Item> execute(Connection connection) throws SQLException {
                 List<Item> items = new ArrayList<>();
-                String query = "select id, name, description from ITEM where description like '%" +
-                        search.getSearchText() + "%'";
+                String query = "select id, name, description from ITEM where description like ?";
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setString(1, "%" + search.getSearchText() + "%");
 
-                LOGGER.log(Level.INFO, "SQL Query " + query);
-                ResultSet rs = connection
-                        .createStatement()
-                        .executeQuery(query);
-
+                LOGGER.log(Level.INFO, "SQL Query " + statement);
+                ResultSet rs = statement.executeQuery();
                 while (rs.next()) {
                     items.add(new Item(rs.getLong("id"), rs.getString("name"), rs.getString("description")));
                 }
