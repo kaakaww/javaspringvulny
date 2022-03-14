501be320162b381afce0900dbd01752001a9c5a9
omaralkhalili <omar.alkhalili@stackhawk.com>
2022-03-14 18:13:25 -0400

    Added additional get mapping

--- src/main/java/hawk/api/jwt/JwtLog4jController.java
+++ src/main/java/hawk/api/jwt/JwtLog4jController.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,18 @@ public class JwtLog4jController {
 
     @GetMapping("/log4j")
     public ResponseEntity logRequest(@RequestParam String text, HttpServletRequest request) {
+        Enumeration<String> headers = request.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String it = headers.nextElement();
+            logger.info("{} = {}", it, request.getHeader(it));
+        }
+        logger.info("Hitting Log4J route");
+        logger.info(text);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/log4j/{text}")
+    public ResponseEntity logRequestPath(@PathVariable String text, HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaderNames();
         while (headers.hasMoreElements()) {
             String it = headers.nextElement();
