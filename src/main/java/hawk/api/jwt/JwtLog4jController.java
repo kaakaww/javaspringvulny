ab36b40f29ad6cdf2800ca6e4bb0755e64b53362
omaralkhalili <omar.alkhalili@stackhawk.com>
2022-03-14 17:30:43 -0400

    Modified log4j route

--- src/main/java/hawk/api/jwt/JwtLog4jController.java
+++ src/main/java/hawk/api/jwt/JwtLog4jController.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
@@ -15,13 +16,14 @@ public class JwtLog4jController {
     private static final Logger logger = LogManager.getLogger(JwtLog4jController.class);
 
     @GetMapping("/log4j")
-    public ResponseEntity logRequest(HttpServletRequest request) {
+    public ResponseEntity logRequest(@RequestParam String text, HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaderNames();
         while (headers.hasMoreElements()) {
             String it = headers.nextElement();
             logger.info("{} = {}", it, request.getHeader(it));
         }
-        logger.info("{}");
+        logger.info("Hitting Log4J route");
+        logger.info(text);
         return ResponseEntity.ok().build();
     }
 
