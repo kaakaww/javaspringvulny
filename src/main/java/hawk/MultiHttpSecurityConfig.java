f02b9be9d722106a996da70436591b9f41f41322
Security Tools Evaluation <99832836+waterlooblonde2@users.noreply.github.com>
2022-03-15 15:40:59 -0400

    Update MultiHttpSecurityConfig.java

--- src/main/java/hawk/MultiHttpSecurityConfig.java
+++ src/main/java/hawk/MultiHttpSecurityConfig.java
@@ -146,6 +146,35 @@ public class MultiHttpSecurityConfig {
                         .permitAll();
         }
     }
+    
+    @Configuration
+    @Order(6)
+    public static class FormLoginWebSecurityConfigurerAdapterNew extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                        .antMatchers(
+                                "/",
+                                "/jwt-auth",
+                                "/token-auth",
+                                "/basic-auth",
+                                "/openapi/**",
+                                "/openapi.yaml",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                    .and()
+                        .formLogin()
+                        .loginPage("/login")
+                        .permitAll()
+                    .and()
+                        .logout()
+                        .logoutSuccessUrl("/")
+                        .permitAll();
+        }
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
