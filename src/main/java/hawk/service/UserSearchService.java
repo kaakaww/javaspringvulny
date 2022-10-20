af003caa1e4e3cb223e18de43f11506796138770
Dan Hopkins <danielhopkins@gmail.com>
2022-10-20 10:02:55 -0600

    Update UserSearchService.java
    If you do try to "fix" this the code won't compile without a corresponding import. I figured keeping all the changes local to the "commented" section was a better way to go.

--- src/main/java/hawk/service/UserSearchService.java
+++ src/main/java/hawk/service/UserSearchService.java
@@ -37,7 +37,7 @@ public class UserSearchService {
 
             /* The righter way, should probably use built in Data Model for this, but this is safe
             String query = "select id, name, description from ITEM where description like ?";
-            PreparedStatement statement = connection.prepareStatement(query);
+            java.sql.PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, "%" + search.getSearchText() + "%");
             LOGGER.log(Level.INFO, "SQL Query {0}",  statement);
             ResultSet rs = statement.executeQuery();
