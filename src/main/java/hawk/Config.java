80ef2e7c2f169293b3e9767f9b977472d1dcd24b
Brandon Ward <brandon.ward@stackhawk.com>
2025-02-12 12:58:22 -0700

    Initial Merging of Hotel Controller into JSV

--- src/main/java/hawk/Config.java
+++ src/main/java/hawk/Config.java
@@ -3,6 +3,7 @@ package hawk;
 import hawk.service.SearchService;
 import hawk.service.UserSearchService;
 import hawk.service.UserService;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaRepositories
+@EntityScan(basePackages = "hawk.hotel")
 public class Config implements WebMvcConfigurer {
     @Bean
     public SearchService searchService(){
---
+++ src/main/java/hawk/hotel/domain/Building.java
@@ -0,0 +1,78 @@
+package hawk.hotel.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "building")
+public class Building {
+
+    @Id
+    @GeneratedValue()
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany
+    @JoinTable(
+            name = "building_staff",
+            joinColumns = @JoinColumn(name = "building_id"),
+            inverseJoinColumns = @JoinColumn(name = "staff_id")
+    )
+    private List<Staff> staff;
+
+    @Column(columnDefinition = "jsonb")
+    private String blahs;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Staff> getStaff() {
+        return staff;
+    }
+
+    public void setStaff(List<Staff> staff) {
+        this.staff = staff;
+    }
+
+    public void setBlahs(List<Integer> blahs) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            this.blahs = objectMapper.writeValueAsString(blahs);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting numbers to JSON", e);
+        }
+    }
+
+    public List<Integer> getBlahs() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(this.blahs, objectMapper.getTypeFactory().constructCollectionType(List.class, Integer.class));
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting JSON to numbers", e);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
